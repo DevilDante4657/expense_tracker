cf874197d5fd473c3bcd3a9d51c83d58638db1a9
DevilDante4657 <ssahejprnyit.edu>
2025-10-16 11:51:04 -0400

    added scaffolding tot he modal and added  textfeild for input

---
+++ lib/new_expense.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class NewExpense extends StatefulWidget{
+  const NewExpense({super.key});
+  
+  @override
+  State<NewExpense> createState() {
+    return _NewExpensesState();
+  }
+  
+}
+class _NewExpensesState extends State<NewExpense>{
+  final _titleController = TextEditingController();
+
+  void dispose(){
+    _titleController.dispose();
+    super.dispose();
+  }
+
+  Widget build(BuildContext context){
+    return Padding(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            controller: _titleController,
+            maxLength: 50,
+            keyboardType: TextInputType.name,
+            decoration: InputDecoration(
+              label: Text("Title")
+            ),
+          ),
+            Row(
+              children: [
+                ElevatedButton(
+                onPressed: (){
+                  print(_titleController.text);
+                  }, 
+                child: Text("Save Expense"))
+              ],
+              )
+        ],
+      )
+      );
+  }
+}
