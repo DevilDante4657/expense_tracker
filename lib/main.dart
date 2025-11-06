ca11361863a8b655297197223ba3ead410563fb7
DevilDante4657 <kingdante4657@gmail.com>
2025-11-06 12:26:00 -0500

    added color scheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,46 @@
 import 'package:expense_tracker/widgets/expenses_list/expenses.dart';
 import 'package:flutter/material.dart';
+
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor:const Color.fromARGB(255, 96, 59, 181),
+);
+
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: Color.fromARGB(255, 5, 99, 125),
+);
 void main(){
   runApp(MaterialApp(
+    themeMode: ThemeMode.system,
+    darkTheme: ThemeData.dark().copyWith(
+      colorScheme: kDarkColorScheme,
+      cardTheme: CardThemeData().copyWith(
+        color: kDarkColorScheme.secondaryContainer,
+        margin: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 8,
+        ),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: kColorScheme.primaryContainer,
+          foregroundColor: kColorScheme.onPrimaryContainer,
+        ),
+      ),
+    ),
     theme: ThemeData().copyWith(
-      scaffoldBackgroundColor: Color.fromARGB(255, 220, 189, 252),
+      colorScheme: kColorScheme,
+      appBarTheme: const AppBarTheme().copyWith(
+        backgroundColor: kColorScheme.onPrimaryContainer,
+        foregroundColor: kColorScheme.primaryContainer,
+      ),
+      textTheme: ThemeData().textTheme.copyWith(
+        titleLarge: TextStyle(
+          fontWeight: FontWeight.normal,
+          color: kColorScheme.onSecondaryContainer,
+          fontSize: 14,
+        ),
+      ),
     ),
     home:Expenses()));
 }
