9768c023c0d0de12a05dacdf006ff28d065af994
DevilDante4657 <kingdante4657@gmail.com>
2025-12-01 16:21:34 -0500

    last commit fixed the null error by adding a missing elevated button them

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,15 @@ void main(){
         backgroundColor: kColorScheme.onPrimaryContainer,
         foregroundColor: kColorScheme.primaryContainer,
       ),
+      cardTheme: CardThemeData().copyWith(
+        color: kColorScheme.secondaryContainer,
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8)
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: kColorScheme.primaryContainer,
+        ),
+      ),
       textTheme: ThemeData().textTheme.copyWith(
         titleLarge: TextStyle(
           fontWeight: FontWeight.normal,
